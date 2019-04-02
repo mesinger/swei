9a1d424f2e29bb4e144df11cd38a2ad0991774b6
karl <karl.bittner@outlook.com>
2019-04-03 00:37:45 +0200

    Added functionality to add images or empty placeholder objects to imagescroll

--- src/main/java/ui/Imagescroll.java
+++ src/main/java/ui/Imagescroll.java
@@ -1,10 +1,20 @@
 package ui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
 import java.io.IOException;
 
 public class Imagescroll extends ScrollPane {
+
+    @FXML
+    private HBox imageBox;
+
     public Imagescroll() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
                 "imagescroll.fxml"));
@@ -16,5 +26,31 @@ public class Imagescroll extends ScrollPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        // TODO: Remove after testing
+        for (int i = 0; i < 10; i++) {
+            //addImage(new Image("file:///home/karl/Data/Technikum/SEM4/SWEI/swei/procedurally.png"));
+            addPlaceholderBox();
+        }
+    }
+
+    private void addPlaceholderBox() {
+        AnchorPane anchorPane = new AnchorPane();
+
+        // Bind the width to the height of the Imagescroll to simulate images with 1:1 aspect ratio
+        anchorPane.prefWidthProperty().bind(heightProperty());
+
+        imageBox.getChildren().add(anchorPane);
+    }
+
+    public void addImage(Image img) {
+        ImageView imageView = new ImageView(img);
+        imageView.fitHeightProperty().bind(imageBox.heightProperty());
+        imageView.setPreserveRatio(true);
+
+        // TODO: This ignores the bottom scrollbar -> images are slightly too high, vertical scrollbar appears
+        imageView.fitHeightProperty().bind(heightProperty());
+
+        imageBox.getChildren().add(imageView);
     }
 }
