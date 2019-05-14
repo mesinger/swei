2d31f2b22fda36fdebfd95b235c85371ccb8d903
karl <karl.bittner@outlook.com>
2019-05-14 09:11:14 +0200

    Image proxy loads image when needed

--- src/main/java/ui/Imagescroll.java
+++ src/main/java/ui/Imagescroll.java
@@ -1,19 +1,30 @@
 package ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Imagescroll extends ScrollPane {
 
     @FXML
     private HBox imageBox;
+
+    private ObservableList<Node> visibleNodes = FXCollections.observableArrayList();
 
     public Imagescroll() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
@@ -28,14 +39,31 @@ public class Imagescroll extends ScrollPane {
         }
 
         // TODO: Remove after testing
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000; i++) {
             //addImage(new Image("file:///home/karl/Data/Technikum/SEM4/SWEI/swei/procedurally.png"));
             addPlaceholderBox();
         }
+
+        vvalueProperty().addListener((obs) -> checkVisible());
+        hvalueProperty().addListener((obs) -> checkVisible());
+
+        visibleNodes.addListener((ListChangeListener<Node>) c -> {
+
+            for (Node node : visibleNodes) {
+
+                Pane pane = (Pane)node;
+
+                if ((pane).getChildren().size() == 0) {
+                    addImage(new Image("file:///home/karl/Data/Technikum/SEM4/SWEI/swei/procedurally.png"), pane);
+                }
+
+            }
+
+        });
     }
 
     private void addPlaceholderBox() {
-        AnchorPane anchorPane = new AnchorPane();
+        AnchorPane anchorPane = new ProxyAnchorPane();
 
         // Bind the width to the height of the Imagescroll to simulate images with 1:1 aspect ratio
         anchorPane.prefWidthProperty().bind(heightProperty());
@@ -43,7 +71,7 @@ public class Imagescroll extends ScrollPane {
         imageBox.getChildren().add(anchorPane);
     }
 
-    public void addImage(Image img) {
+    public void addImage(Image img, Pane pane) {
         ImageView imageView = new ImageView(img);
         imageView.fitHeightProperty().bind(imageBox.heightProperty());
         imageView.setPreserveRatio(true);
@@ -51,6 +79,27 @@ public class Imagescroll extends ScrollPane {
         // TODO: This ignores the bottom scrollbar -> images are slightly too high, vertical scrollbar appears
         imageView.fitHeightProperty().bind(heightProperty());
 
-        imageBox.getChildren().add(imageView);
+        pane.getChildren().add(imageView);
+    }
+
+    private void checkVisible() {
+        visibleNodes.setAll(getVisibleNodes());
+    }
+
+    private List<Node> getVisibleNodes() {
+        List<Node> visibleNodes = new ArrayList<>();
+        Bounds paneBounds = localToScene(getBoundsInParent());
+
+        if (getContent() instanceof Parent) {
+            for (Node n : ((Parent) getContent()).getChildrenUnmodifiable()) {
+                Bounds nodeBounds = n.localToScene(n.getBoundsInLocal());
+                if (paneBounds.intersects(nodeBounds)) {
+                    visibleNodes.add(n);
+                }
+            }
+        }
+
+        return visibleNodes;
     }
 }
+
---
+++ src/main/java/ui/ProxyAnchorPane.java
@@ -0,0 +1,9 @@
+package ui;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+
+public class ProxyAnchorPane extends AnchorPane {
+
+}
