f1420af1946928459679c83b494450d024403d5f
karl <karl.bittner@outlook.com>
2019-05-20 17:25:29 +0200

    new Photographer validation

--- src/main/java/ui/PhotographersController.java
+++ src/main/java/ui/PhotographersController.java
@@ -1,4 +1,38 @@
 package ui;
 
-public class PhotographersController {
+import javafx.beans.value.ChangeListener;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class PhotographersController implements Initializable {
+    public ListView photographerList;
+    public Button addPhotographerButton;
+    public TextField firstName;
+    public TextField lastName;
+    public DatePicker dateOfBirth;
+    public TextField notes;
+    public Button saveButton;
+
+    private boolean allFieldsValid() {
+        return (!lastName.getText().isEmpty() && dateOfBirth.getValue() != null && dateOfBirth.getValue().isBefore(LocalDate.now()));
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        saveButton.setDisable(true);
+
+        ChangeListener textChangedListener = (observable, oldValue, newValue) -> saveButton.setDisable(!allFieldsValid());
+
+        firstName.textProperty().addListener(textChangedListener);
+        lastName.textProperty().addListener(textChangedListener);
+        dateOfBirth.valueProperty().addListener(textChangedListener);
+    }
 }
